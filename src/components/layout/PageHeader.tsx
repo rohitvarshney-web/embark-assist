8f72ddfc0df0ddf1165dbf6b317e99dd376a5b27
Rohit Varshney <rohit.varshney@stampmyvisa.com>
2025-12-17 15:17:08 +0530

    Update PageHeader.tsx

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Container } from "./Container";
 
-const LOGO_URL = "https://ik.imagekit.io/9pjdbdyeq/StampMyVisa%20Brand%20Assets/Logo/Logo-Dark/Logo-Dark.svg?updatedAt=1754036741863";
+const LOGO_URL = "https://stampmyvisa.com/assets/images/logo-with-name.svg";
 
 interface PageHeaderProps {
   showNavigation?: boolean;
